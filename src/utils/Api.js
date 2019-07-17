bd2b9b365fb518df81c8cf73334030bdbebd9f50
nastromo <nastromo@Solaris.local>
2019-07-17 16:36:38 +0300

    url fix

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let url = `https://api.wellcom.us`;
+let url = `https://api.mywellcom.us`;
 if (window.location.href.includes(`localhost`)) {
     url = `http://localhost:4000`;
 }
